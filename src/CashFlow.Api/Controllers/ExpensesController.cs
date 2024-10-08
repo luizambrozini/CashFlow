6acc490721473fa9ac4c234c86c50031bee37ac9
luizambrozini <luizambrozini@gmail.com>
2024-10-07 21:21:28 -0300

    CashFlow

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Comunication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Reguster([FromBody] RequestRegisterExpeseJson requestRegisterExpeseJson)
+        {
+            return Created();
+        }
+    }
+}
