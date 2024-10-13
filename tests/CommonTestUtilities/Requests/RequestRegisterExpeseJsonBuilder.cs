18017e79b3aa02643cdc733f057d465fa83fddf5
luizambrozini <luizambrozini@gmail.com>
2024-10-13 12:46:47 -0300

    CashFlow

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpeseJsonBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Bogus;
+using CashFlow.Comunication.Enums;
+using CashFlow.Comunication.Requests;
+
+namespace CommonTestUtilities.Requests
+{
+    public class RequestRegisterExpeseJsonBuilder
+    {
+        public static RequestRegisterExpeseJson Build()
+        {
+            return new Faker<RequestRegisterExpeseJson>()
+                 .RuleFor(r => r.Title, f => f.Commerce.ProductName())
+                 .RuleFor(r => r.Description, f => f.Commerce.ProductDescription())
+                 .RuleFor(r => r.Date, f => f.Date.Past())
+                 .RuleFor(r => r.PaymentType, f => f.PickRandom<PaymentType>())
+                 .RuleFor(r => r.Amount, f => f.Finance.Amount(50, 200));
+        }
+    }
+}
---
+++ tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validators.Test.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+        [Fact]
+        public void Success()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpeseJsonBuilder.Build();
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Error_Title_Empty()
+        {
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpeseJsonBuilder.Build();
+            request.Title = string.Empty;
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessage.TITLE_REQUIRED));
+        }
+    }
+}
