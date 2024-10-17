516c862a9b7c177a04f9bfb81a4c90e571a85524
luizambrozini <luizambrozini@gmail.com>
2024-10-17 14:24:17 -0300

    CashFlow

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Domain.Reports;
 using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
 {
@@ -12,9 +13,13 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
             workbook.Style.Font.FontSize = 12;
             workbook.Style.Font.FontName = "Times New Roman";
 
-            var woeksheet = workbook.Worksheets.Add($"Report_Expenses_{month.ToString("Y")}");
+            var worksheet = workbook.Worksheets.Add($"Report_Expenses_{month.ToString("Y")}");
+            AddHeader(worksheet);
 
+            var file = new MemoryStream();
+            workbook.SaveAs(file);
 
+            return file.ToArray();
         }
 
         private void AddHeader(IXLWorksheet worksheet)
@@ -24,6 +29,16 @@ namespace CashFlow.Application.UseCases.Expenses.Reports.Excel
             worksheet.Cell("C1").Value = ResourceReportGenerationMessages.DATE;
             worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
             worksheet.Cell("E1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+
+            worksheet.Cells("A1:E1").Style.Font.Bold = true;
+            worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#F5c2b6");
+
+            worksheet.Cell("A1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("B1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+            worksheet.Cell("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+
         }
     }
 }
